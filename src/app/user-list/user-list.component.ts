6adc6ee4dbfe121d70eead847f8b65474e8e5413
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-09-03 17:24:50 +0300

    create user-list component

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+})
+export class UserListComponent {
+  users: any[] = [];
+  searchQuery: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchUsers(1);
+  }
+
+  fetchUsers(page: number): void {
+    const apiUrl = `https://reqres.in/api/users?page=${page}`;
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.users = data.data; // Assuming the user data is under the 'data' property
+    });
+  }
+
+  navigateToUserDetails(userId: number): void {
+    this.router.navigate(['user', userId]);
+  }
+}
