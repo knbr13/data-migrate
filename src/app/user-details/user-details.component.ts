abea4d95572a0e5868483947c3c68b44552dd591
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-09-03 18:48:52 +0300

    update user-details component

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  userId: number = 1;
+  user: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.userId = +params['id'];
+      this.fetchUserDetails(this.userId);
+    });
+  }
+
+  fetchUserDetails(id: number): void {
+    const apiUrl = `https://reqres.in/api/users/${id}`;
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.user = data.data; // Assuming the user data is under the 'data' property
+    });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/users']);
+  }
 }
