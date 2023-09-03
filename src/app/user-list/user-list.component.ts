9837d1d35bc25862fc940bcec731c192fc9e54bc
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-09-03 18:49:10 +0300

    implement OnInit interface

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 @Component({
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
   users: any[] = [];
   searchQuery: string = '';
 
