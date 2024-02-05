e69b7e3fb93ce248c6d9c48cb892124f5c86cf20
knbr13 <abdullahalaadine63@gmail.com>
2024-02-05 21:08:55 +0200

    parse data, then insert to db

--- main.go
+++ main.go
@@ -1,7 +1,115 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("hello world")
+	defer db.Close()
+
+	var tx *sql.Tx
+	if useTx {
+		var err error
+		tx, err = db.Begin()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: starting transaction: %s\n", err.Error())
+			os.Exit(1)
+		}
+		defer tx.Rollback()
+	}
+
+	isdir, err := isDir(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	var files []string
+
+	if isdir {
+		files, err = filesInDir(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+			os.Exit(1)
+		}
+	} else {
+		files = []string{path}
+	}
+
+	for _, file := range files {
+		data, err := openAndReadFile(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+			os.Exit(1)
+		}
+		m := make(map[string]any)
+		err = json.Unmarshal(data, &m)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		for table, data := range m {
+			switch d := data.(type) {
+			case []any:
+				for _, obj := range d {
+					switch v := obj.(type) {
+					case map[string]any:
+						query, values := buildSQLInsertQueryFromMap(v, table)
+						if useTx {
+							err = txInsert(tx, query, values...)
+						} else {
+							err = insert(query, values...)
+						}
+						if err != nil {
+							fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+							if useTx {
+								tx.Rollback()
+								os.Exit(1)
+							}
+						}
+					default:
+						fmt.Fprintf(os.Stderr, "error: %s\n", "unsupported type")
+						if useTx {
+							tx.Rollback()
+							os.Exit(1)
+						}
+					}
+				}
+			case map[string]any:
+				query, values := buildSQLInsertQueryFromMap(d, table)
+				if useTx {
+					err = txInsert(tx, query, values...)
+				} else {
+					err = insert(query, values...)
+				}
+				if err != nil {
+					fmt.Println("error:", err)
+					if useTx {
+						tx.Rollback()
+						os.Exit(1)
+					}
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "unsupported type: %T, values for table names should be object or array of objects.\n", d)
+				if useTx {
+					tx.Rollback()
+					os.Exit(1)
+				}
+			}
+		}
+	}
+	if useTx {
+		err = tx.Commit()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+			os.Exit(1)
+		}
+	}
+	fmt.Println("insertion completed!")
 }
